616caccba67378c2858c4257498a4dbddaad8544
Shaleen Chugh <chugh97@yahoo.com>
2013-12-08 22:19:52 +0000

    Full basket integration with paypal

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,19 @@
 class OrdersController < ApplicationController
   def express
-    response = EXPRESS_GATEWAY.setup_purchase(10,
+    total = Cart.total(session[:session_id])
+    items = Cart.items_in_cart(session[:session_id])
+    response = EXPRESS_GATEWAY.setup_purchase(total * 100,
                                               :ip                => request.remote_ip,
                                               :return_url        => orders_new_url,
-                                              :cancel_return_url => products_url
+                                              :cancel_return_url => products_url,
+                                              currency: 'GBP',
+                                              locale: I18n.locale.to_s.sub(/-/, '_'), #you can put 'en' if you don't know what it means :)
+                                              brand_name: 'e-Shop', #The name of the company
+                                              allow_guest_checkout: 'true',   #payment with credit card for non PayPal users
+        items: items #array of hashes, amount is a price in cents
+
     )
+
     redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
   end
 
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,24 @@
 class Cart < ActiveRecord::Base
   belongs_to :product                   #foreign_key product_id
 
+  def self.total(session_id)
+     self.where('session_id = ?', session_id).sum('unit_price * quantity')
+  end
+
+  def self.items_in_cart(session_id)
+    cart_json = self.where('session_id = ?', session_id).as_json
+
+    cart_json.collect do |line_item|
+      product = line_item['product']
+
+      {
+          :name => product['name'],
+          :number => product['description'],
+          :quantity => line_item['quantity'],
+          :amount => line_item['unit_price'] * 100
+      }
+    end
+  end
 
   def as_json(options={ })
     super({:include => [:product]}.merge(options))
