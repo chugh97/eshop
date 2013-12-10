0c6bb30ffdf8e28c2fedaedea27cf0d88dd7c1d3
Shaleen Chugh <chugh97@yahoo.com>
2013-12-10 00:04:37 +0000

    Seed data scripts added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+category = Category.create!(name: 'Toiletries')
+p1 = Product.create!(name: 'Radox', category: category, description: "Radox Men's shower gel")
+p2 = Product.create!(name: 'Imperial Leather', category: category, description: "Bar Soap 200gm")
+p3 = Product.create!(name: 'Sunsense', category: category, description: "Sunsense sun cream 100gm")
+
+ProductPrice.create!(product: p1, price: 1.99, effective_start_date: 1.day.ago)
+ProductPrice.create!(product: p2, price: 2.69, effective_start_date: 1.day.ago)
+ProductPrice.create!(product: p3, price: 14.99, effective_start_date: 1.day.ago)
+
+
